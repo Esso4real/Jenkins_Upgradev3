7a524b82e95f89adbb1fc606bc6b3ddfcc157db3
Esso4real <81115661+Esso4real@users.noreply.github.com>
2021-04-18 12:53:02 -0600

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,7 +40,7 @@ public class TestGreeter {
   
       @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy, Ernest";
+    String someone = "World, Thanks you making me Happy, Ernest le grand Esso";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
